a694e31f7346797a285ace68e289e5dd5ba15e9a
Dmitri Bagaev <bvdmitri@gmail.com>
2019-03-29 16:07:56 +0300

    multiple parameters update support

---
+++ Examples/TTSPFunction/function/static_sin_r2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dmitri Bagaev on 2019-03-29.
+//
+
+#include "static_sin_r2.h"
---
+++ Examples/TTSPFunction/function/static_sin_r2.h
@@ -0,0 +1,14 @@
+//
+// Created by Dmitri Bagaev on 2019-03-29.
+//
+
+#ifndef INMOST_STATIC_SIN_R2_H
+#define INMOST_STATIC_SIN_R2_H
+
+
+class static_sin_r2 {
+
+};
+
+
+#endif //INMOST_STATIC_SIN_R2_H
